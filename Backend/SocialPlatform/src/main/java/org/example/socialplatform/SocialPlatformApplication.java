41b2df45abe6c96af1b2cd36824fabe211980543
adelin4702 <adelinpricop124@gmail.com>
2025-03-21 21:55:44 +0200

    setup

--- Backend/SocialPlatform/src/main/java/org/example/socialplatform/SocialPlatformApplication.java
+++ Backend/SocialPlatform/src/main/java/org/example/socialplatform/SocialPlatformApplication.java
@@ -2,8 +2,10 @@ package org.example.socialplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SocialPlatformApplication {
 
 	public static void main(String[] args) {
